898dbc2ce65f06e96ff18d7252739873d9bea1f8
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2024-04-16 18:14:46 +0300

    form adding exercise

---
+++ frontend/src/components/CreateExercise.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import {Modal, Form, Button} from 'react-bootstrap';
+
+import {instance} from '../api/axios.api';
+
+export const CreateExercise = ({show, handleClose}) => {
+    const [exerciseData, setExerciseData] = useState({
+        name: '',
+        description: '',
+        photo: null,
+    });
+
+    const handleChange = (e) => {
+        const { name, value, files } = e.target;
+        const newValue = name === 'photo' ? (files.length > 0 ? files[0] : null) : value;
+        setExerciseData(prevState => ({
+            ...prevState,
+            [name]: newValue
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('name', exerciseData.name);
+        formData.append('description', exerciseData.description);
+
+        if (exerciseData.photo) {
+            formData.append('photo', exerciseData.photo);
+        }
+
+        instance.post('exercise/', formData).then((resp) => {
+            console.log('Упражнение успешно добавлено:', resp.data);
+            window.location.reload();
+        }).catch((err) => {
+            console.log(err.response?.data);
+        });
+    };
+
+    return (
+        <>
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title >Упражнение</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Label>Название</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="name"
+                            placeholder="Введите название упражнения"
+                            value={exerciseData.name}
+                            onChange={handleChange}
+                        />
+                        <Form.Group className="mt-2">
+                            <Form.Label>Описание</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                name="description"
+                                placeholder="Введите описание упражнения"
+                                value={exerciseData.description}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mt-2">
+                            <Form.Label>Фото</Form.Label>
+                            <Form.Control
+                                type="file"
+                                name="photo"
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Закрыть
+                    </Button>
+                    <Button variant="warning" onClick={handleSubmit}>
+                        Сохранить
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
--- frontend/src/pages/ExercisePage.jsx
+++ frontend/src/pages/ExercisePage.jsx
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react';
 import {Button} from 'react-bootstrap';
 
 import {Exercise} from '../components/Exercise';
+import {CreateExercise} from '../components/CreateExercise';
 import {instance} from '../api/axios.api';
 
 export const ExercisePage = () => {
     const [exercises, setExercises] = useState([])
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
         instance.get('exercise/').then((resp) => {
@@ -16,16 +21,16 @@ export const ExercisePage = () => {
         });
     }, [])
 
-
     return (
         <>
             <div className="d-flex align-items-center text-center" style={{minWidth: "600px"}}>
                 <h3 className="col-4">Упражнения</h3>
-                <Button variant="warning">Добавить</Button>
+                <Button variant="warning" onClick={handleShow}>Добавить</Button>
             </div>
             {exercises.map((exercise) => (
                 <Exercise exercise={exercise} />
             ))}
+            <CreateExercise show={show} handleClose={handleClose} />
         </>
     )
 }

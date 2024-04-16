748b532c7490535e0caa77592c71ccc0db973e2e
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2024-04-16 16:16:17 +0300

    fix img in Exercise

--- frontend/src/components/Exercise.jsx
+++ frontend/src/components/Exercise.jsx
@@ -1,17 +1,20 @@
 
 
 export const Exercise = (exercise) => {
+    const {name, photo, photo_url, description} = exercise.exercise
+    const imageUrl = photo_url ? photo_url : photo
+
     return (
         <>
-            <h4 className="m-3">{exercise.exercise.name}</h4>
+            <h4 className="m-3">{name}</h4>
             <div className="bg-light d-flex mt-3" style={{minWidth: "500px"}}>
                 <img
-                    src={exercise.exercise.photo}
+                    src={imageUrl}
                     width="130"
                     alt="Logo"
                     className="m-2"
                 />
-                <p className="m-2 overflow-hidden">{exercise.exercise.description}</p>
+                <p className="m-2 overflow-hidden">{description}</p>
             </div>
         </>
     )
